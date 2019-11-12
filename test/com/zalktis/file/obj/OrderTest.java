c920d875b2ae56c8062193e5b4c70b5dee77f236
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2019-11-12 18:18:23 +0200

    add Order and Task tests
    Signed-off-by: Artūrs Jānis Ņikitins <ajnikitins@gmail.com>

---
+++ test/com/zalktis/file/obj/OrderTest.java
@@ -0,0 +1,73 @@
+package com.zalktis.file.obj;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.zalktis.file.exceptions.DateBeforeTodayException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OrderTest {
+
+  Order order;
+
+  @BeforeEach
+  void setUp() {
+    order = new Order();
+  }
+
+  @Test
+  void setAndGetID() {
+    order.setID(0);
+    assertEquals(0, order.getID());
+  }
+
+  @Test
+  void setAndGetName() {
+    order.setName("Ad page");
+    assertEquals("Ad page", order.getName());
+  }
+
+  @Test
+  void setAndGetCustomerName() {
+    order.setCustomerName("VELVE");
+    assertEquals("VELVE", order.getCustomerName());
+  }
+
+  @Test
+  void setAndGetDetails() {
+    order.setDetails("Many");
+    assertEquals("Many", order.getDetails());
+  }
+
+  @Test
+  void setAndGetCompletionDate() {
+    order.setCompletionDate(LocalDate.of(2020,12,3));
+    assertEquals(LocalDate.of(2020, 12, 3), order.getCompletionDate());
+  }
+
+  @Test
+  void setCompletionDateAndFail() {
+    assertThrows(DateBeforeTodayException.class, () -> order.setCompletionDate(LocalDate.of(1995,12,3)));
+  }
+
+  @Test
+  void getTasks() {
+    assertEquals(new ArrayList<Task>(), order.getTasks());
+  }
+
+  @Test
+  void addAndFindAndRemoveTask() {
+    order.setCompletionDate(LocalDate.of(2020, 12, 3));
+    order.addTask("Print", "None", 4);
+
+    Task task = new Task(0, 0, "Print", "None", LocalDate.of(2020, 12, 3), 4);
+    assertEquals(task, order.findTaskByID(0));
+
+    order.removeTask(0);
+    assertNull(order.findTaskByID(0));
+  }
+}
---
+++ test/com/zalktis/file/obj/TaskTest.java
@@ -0,0 +1,75 @@
+package com.zalktis.file.obj;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.zalktis.file.exceptions.DateBeforeTodayException;
+import java.time.LocalDate;
+import java.time.Period;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TaskTest {
+
+  Task task;
+
+  @BeforeEach
+  void setUp() {
+    task = new Task(0, 0, "Print", "None", LocalDate.of(2020, 12, 3), 4);
+  }
+
+  @Test
+  void getID() {
+    assertEquals(0, task.getID());
+  }
+
+  @Test
+  void getParentID() {
+    assertEquals(0, task.getParentID());
+  }
+
+  @Test
+  void getAndSetName() {
+    assertEquals("Print", task.getName());
+    task.setName("Buy paper");
+    assertEquals("Buy paper", task.getName());
+  }
+
+  @Test
+  void getAndSetDetails() {
+    assertEquals("None", task.getDetails());
+    task.setDetails("Many");
+    assertEquals("Many", task.getDetails());
+  }
+
+  @Test
+  void setDaysBeforeOrderAndFail() {
+    assertThrows(DateBeforeTodayException.class, () -> task.setDaysBeforeOrder(9999));
+  }
+
+  @Test
+  void getAndSetDaysBeforeOrder() {
+    assertEquals(4, task.getDaysBeforeOrder());
+    task.setDaysBeforeOrder(6);
+    assertEquals(6, task.getDaysBeforeOrder());
+  }
+
+  @Test
+  void setOrderCompletionDateAndFail() {
+    assertThrows(DateBeforeTodayException.class, () -> task.setOrderCompletionDate(LocalDate.of(1920, 1, 1)));
+  }
+
+  @Test
+  void getAndSetOrderCompletionDate() {
+    assertEquals(LocalDate.of(2020, 12, 3), task.getOrderCompletionDate());
+    task.setOrderCompletionDate(LocalDate.of(2021, 9, 4));
+    assertEquals(LocalDate.of(2021, 9, 4), task.getOrderCompletionDate());
+  }
+
+  @Test
+  void getAndCompletionDate() {
+    assertEquals(LocalDate.of(2020, 11, 27), task.getCompletionDate());
+    task.setDaysBeforeOrder(5);
+    assertEquals(LocalDate.of(2020, 11, 26), task.getCompletionDate());
+  }
+
+}
