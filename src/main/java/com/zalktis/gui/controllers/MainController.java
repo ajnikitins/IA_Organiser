46cf76cc10cd2384b6c339406fcc7fdaa7dd065e
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2019-12-13 11:23:24 +0200

    integrate OrderList and ImminentTaskList into MainController
    Signed-off-by: Artūrs Jānis Ņikitins <ajnikitins@gmail.com>

--- src/main/java/com/zalktis/gui/controllers/MainController.java
+++ src/main/java/com/zalktis/gui/controllers/MainController.java
@@ -1,6 +1,10 @@
+// TODO: https://stackoverflow.com/questions/9966136/javafx-periodic-background-task
+
 package com.zalktis.gui.controllers;
 
+import com.zalktis.file.FileSystem;
 import com.zalktis.gui.components.ImminentTaskList;
+import com.zalktis.gui.components.OrderList;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -15,8 +19,24 @@ public class MainController implements Initializable {
   @FXML
   private ImminentTaskList imminentTaskList;
 
+  @FXML
+  private OrderList orderList;
+
   @Override
   public void initialize(URL url, ResourceBundle rb) {
+    FileSystem fileSystem = FileSystem.load();
+    fileSystem.setOnChange(() -> {
+      imminentTaskList.update();
+      orderList.update();
+    });
+    imminentTaskList.setFileSystem(fileSystem);
+    orderList.setFileSystem(fileSystem);
+
+    imminentTaskList.setOnTitleClick(order -> {
+      mainPane.getSelectionModel().select(1);
+      orderList.findOrderPaneByID(order.getID()).setExpanded(true);
+    });
+
     mainPane.tabMinWidthProperty().bind(mainPane.widthProperty().divide(mainPane.getTabs().size()).subtract(20));
   }
 }
